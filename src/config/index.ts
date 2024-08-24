b82fcb7d1fcca0845b93592f7025b494e223e35a
ArnoldsK <arnolds0klavenieks@gmail.com>
2024-08-24 18:49:00 +0300

    [Fix] fix tsconfig issue

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,16 +6,17 @@ import jsonConfig from "../../config.json"
 export const config: Config = z
   .object({
     app: z.object({
-      url: z.string().url(),
-      port: z.number(),
+      url: z.string().url().min(1),
+      port: z.number().int().positive(),
     }),
     auth: z.object({
       clients: z.array(
         z.object({
-          name: z.string().toLowerCase(),
-          token: z.string(),
+          name: z.string().toLowerCase().min(1),
+          token: z.string().min(1),
         }),
       ),
     }),
   })
+
   .parse(jsonConfig)
